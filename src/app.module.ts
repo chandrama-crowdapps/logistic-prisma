97876d4b760c1780323c9dd6857e2e2ba3533c44
chandrama-surfair <163103361+chandrama-surfair@users.noreply.github.com>
2024-04-08 07:54:26 +0530

    test app

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import {
+  Module,
+  NestModule,
+  RequestMethod,
+  MiddlewareConsumer,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+@Module({
+  imports: [PrismaModule, ArticlesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'articles', method: RequestMethod.ALL });
+  }
+}
